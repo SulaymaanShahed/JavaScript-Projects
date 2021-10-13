fa97ecc18ec31247cafa722280b17852f6d9df30
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-10-13 13:27:58 -0700

    Comments
    Comments added to JS file.

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
-function myFunction() {
-    var sentence = "I am learning";
-    sentence += " a lot from this program!";
-    document.getElementById("Concatenate").innerHTML = sentence;
+function myFunction() { //Creating a function.
+    var sentence = "I am learning"; //Creating a variable with a string attached "I am learning"
+    sentence += " a lot from this program!"; //Variable concatenating with another string.
+    document.getElementById("Concatenate").innerHTML = sentence; //This function is used to grab an element from the ID "Concatenate"
 }
