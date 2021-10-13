3e600a7162f837f858e7c44ba57efa4c44a8438c
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-10-13 09:50:12 -0700

    New additions
    New additions to Repository.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+window.alert("Hello, World!"); //This makes a window pop out like an alert that says the text held within the parantheses.
+document.write('Hello, World! '); //Text written in the parantheses gets written on the page.
+
+document.write(3+3); //This is an expression that makes the number 6.
+
+var S = " Concatenated" + " String"; //We created the variable S.
+window.alert(S); //This causes the above variable to show on a tiny pop up window and shows the text in the ().
+
+var A = "This is a string"; //Variable created.
+
+var A = A.fontcolor("blue"); //Fontcolor change for string.
+document.write(" I made a string.", //Writes down text held within parantheses.
+    ' Don\'t get me wrong, please understand.\ I am doing my best. '); //Concatenated string.
+
+
+var B = "I am"; //Created a variable B with string attached.
+var Y = "Bored."; //Created a variable Y with string attached.
+var Y = str.fontcolor("green"); //Changes color of variables string.
+document.write(B+Y); //Executed the concatenation of the variables because of the + operator.
+
