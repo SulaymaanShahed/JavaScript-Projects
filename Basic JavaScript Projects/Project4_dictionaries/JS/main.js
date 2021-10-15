ca80cb254a67575d42a8022a72cd864ac29ff8a8
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-10-15 14:19:57 -0700

    Dictionary
    We are doing dictionaries with JavaScript objects here.

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,24 @@
+function my_Animals() {
+    var avian_Animal = {
+        Species:"C. brachyrhynchos",
+        Genus:"Corvus",
+        Color:"Brown",
+        Age:2,
+        Sound:"Caw!",
+    };
+    var land_Animal = {
+        Species:"A. jubatus",
+        Genus:"Acinonyx",
+        Color:"Orange",
+        Age:4,
+        Sound:"Chirp!",
+    };
+    var ocean_Animal = {
+        Species:"M. birostris",
+        Genus:"Mobula",      
+        Color:"Black",
+        Age:30,
+        Sound:"Creaking!",
+    };
+    document.getElementById("Animals").innerHTML = Animal.Sound;
+}
